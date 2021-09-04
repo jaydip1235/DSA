91b705c7f7f69d288f388d5a4a9e84d1c582a0fa
jaydip1235 <jaydipdeyalok@gmail.com>
2021-09-04 19:11:10 +0530

    issymmetric

---
+++ BinaryTree/Easy/IsSymmetric.java
@@ -0,0 +1,15 @@
+// 
+import java.util.*;
+public class IsSymmetric {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null)return true;
+        return isMirror(root, root);
+    }
+    
+    public boolean isMirror(TreeNode node1, TreeNode node2){
+        if(node1==null || node2==null)return node1==node2;
+        if(node1.val != node2.val)return false;
+        return (isMirror(node1.left,node2.right) && isMirror(node1.right,node2.left));
+    }
+
+}
