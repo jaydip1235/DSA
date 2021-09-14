61d959f4306b912a9dcc9000c15c924d81b4c54d
jaydip1235 <jaydipdeyalok@gmail.com>
2021-09-14 20:38:43 +0530

    CopyListWithRandomPointer

--- LinkedList/Medium/CopyListWithRandomPointer.java
+++ LinkedList/Medium/CopyListWithRandomPointer.java
@@ -23,5 +23,66 @@ public class CopyListWithRandomPointer {
             p2=p2.next;
         }
         return dh.next;
+
+
+
+        // Optimized implementation
+        public Node copyRandomList(Node head) {
+            if(head==null)return null;
+            Node curr = head;
+    
+            while (curr != null) {
+                Node n = curr.next;
+    
+                Node nn = new Node(curr.val);
+    
+                curr.next = nn;
+                nn.next = n;
+    
+                curr = n;
+            }
+    
+    
+            //2. set random pointers
+            Node p1 = head;
+    
+            while (p1 != null) {
+                Node p2 = p1.next;
+    
+                if (p1.random == null) {
+                    p2.random = null;
+                } else {
+                    p2.random = p1.random.next;
+                }
+    
+                p1 = p1.next.next;
+    
+            }
+    
+            //3. segregate old and new list
+            Node cllh = head.next; //copied linked list head
+    
+            Node h1 = head;
+            Node h2 = cllh;
+    
+            while (h1 != null) {
+                Node n1 = h2.next;
+    
+                Node n2 = null;
+    
+                if (n1 != null) {
+                    n2 = n1.next;
+                }
+    
+                h1.next = n1;
+                h2.next = n2;
+    
+                h1 = n1;
+                h2 = n2;
+            }
+    
+    
+            return cllh;
     }
+       
 }
