e7ad85bf60bedc24757c99b1864bc03f353b21b8
jaydip1235 <jaydipdeyalok@gmail.com>
2021-10-02 14:17:19 +0530

    Binary Tree with pre and post

---
+++ BinaryTree/Medium/TreeFromPrePost.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/
+import java.util.* ;
+public class TreeFromPrePost {
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        int n = preorder.length;
+        return helper(preorder, 0, n - 1, postorder, 0, n - 1);
+    }
+
+    public static TreeNode helper(int[] preorder, int prs, int pre, int[] postorder, int pos, int poe) {
+        if (pos > poe) {
+            return null;
+        }
+
+        if (prs == pre) {
+            return new TreeNode(preorder[prs]);
+        }
+
+        TreeNode node = new TreeNode(preorder[prs]);
+
+        int val = preorder[prs + 1];
+        int idx = -1;
+
+        for (int i = pos; i <= poe; i++) {
+            if (postorder[i] == val) {
+                idx = i;
+                break;
+            }
+        }
+
+        int cls = idx - pos + 1;
+
+        node.left = helper(preorder, prs + 1, prs + cls, postorder, pos, idx);
+        node.right = helper(preorder, prs + cls + 1, pre, postorder, idx + 1, poe - 1);
+
+        return node;
+    }
+}
