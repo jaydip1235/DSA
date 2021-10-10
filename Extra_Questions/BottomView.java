bd115bc05e545e5badc67a0dbc94059108ae5c84
jaydip1235 <jaydipdeyalok@gmail.com>
2021-10-10 19:55:15 +0530

    Bottom View of binary tree

---
+++ Extra_Questions/BottomView.java
@@ -0,0 +1,56 @@
+//Given a binary tree, print the bottom view from left to right.
+//A node is included in bottom view if it can be seen when we look at the tree from bottom.
+import java.util.*;
+public class BottomView {
+    public static class verticalPair {
+        TreeNode node = null;
+        int hl = 0; // horizontal Level
+
+        verticalPair(TreeNode node, int hl) {
+            this.node = node;
+            this.hl = hl;
+        }
+    }
+
+    // ans = {minHl,maxHL}
+    public static void width(TreeNode root, int hl, int[] ans) {
+        if (root == null)
+            return;
+
+        ans[0] = Math.min(hl, ans[0]);
+        ans[1] = Math.max(hl, ans[1]);
+
+        width(root.left, hl - 1, ans);
+        width(root.right, hl + 1, ans);
+    }
+
+    public static ArrayList<Integer> BottomView(TreeNode root) {
+        LinkedList<verticalPair> que = new LinkedList<>();
+
+        int[] minMax = new int[2];
+        width(root, 0, minMax);
+        int length = minMax[1] - minMax[0] + 1;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < length; i++)
+            ans.add(0);
+
+        que.addLast(new verticalPair(root, -minMax[0]));
+
+        while (que.size() != 0) {
+            int size = que.size();
+            while (size-- > 0) {
+                verticalPair rp = que.removeFirst();
+
+                ans.set(rp.hl, rp.node.val);
+
+                if (rp.node.left != null)
+                    que.addLast(new verticalPair(rp.node.left, rp.hl - 1));
+
+                if (rp.node.right != null)
+                    que.addLast(new verticalPair(rp.node.right, rp.hl + 1));
+            }
+        }
+
+        return ans;
+    }
+}
