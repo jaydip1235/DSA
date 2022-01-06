7ecbb98e29a79d707e5ee99e6eda21863fb7eb63
jaydip1235 <jaydipdeyalok@gmail.com>
2022-01-06 18:03:38 +0530

    Number Of Employees Under Every Manager

---
+++ HashMap/Medium/NoOfEmployees.java
@@ -0,0 +1,68 @@
+// https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/hashmap-and-heaps/number-of-employees-under-every-manager-official/ojquestion
+import java.io.*;
+import java.util.*;
+
+public class NoOfEmployees {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        HashMap < String, String > map = new HashMap < > (); //employee vs manager
+        String ceo = "";
+
+        while (n-- > 0) {
+            String emp = scn.next();
+            String man = scn.next();
+
+            map.put(emp, man); 
+        }
+
+
+        HashMap < String, ArrayList < String >> gt = new HashMap < > (); //manager vs (direct employees under manager)
+
+        for (String emp: map.keySet()) {
+            String man = map.get(emp);
+
+            if (emp.equals(man) == true) {
+                ceo = emp;
+                ArrayList < String > list = gt.getOrDefault(man, new ArrayList < > ());
+                gt.put(emp, list);
+                continue;
+            }
+
+            if (gt.containsKey(emp) == false) {
+                gt.put(emp, new ArrayList < > ());
+            }
+
+
+            ArrayList < String > list = gt.getOrDefault(man, new ArrayList < > ());
+            list.add(emp);
+            gt.put(man, list);
+
+        }
+
+        HashMap < String, Integer > res = new HashMap < > ();
+        getSize(ceo, gt, res);
+
+
+        for (String key: res.keySet()) {
+            System.out.println(key + " " + res.get(key));
+        }
+    }
+
+    public static int getSize(String root, HashMap < String, ArrayList < String >> gt, HashMap < String, Integer > res) {
+
+        int s = 0;
+
+        for (String child: gt.get(root)) {
+            int cfs = getSize(child, gt, res);
+            s += cfs;
+        }
+
+        res.put(root, s);
+        return s + 1;
+
+    }
+
+}
