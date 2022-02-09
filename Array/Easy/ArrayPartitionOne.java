3648048c7e309ebd2cf146ac028e1e2edf808e56
jaydip1235 <jaydipdeyalok@gmail.com>
2022-02-09 12:41:03 +0530

    Array Partition 1

---
+++ Array/Easy/ArrayPartitionOne.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/array-partition-i/
+import java.util.*;
+public class ArrayPartitionOne{
+      public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum=0;
+        for(int i=0;i<nums.length;i+=2)sum+=nums[i];
+        return sum;
+        
+    }
+}
