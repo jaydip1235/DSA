aed6730de6cb62233790a5e18006c28c5ca0d3f1
jaydip1235 <jaydipdeyalok@gmail.com>
2022-02-27 18:08:33 +0530

    2 keys keyboard

---
+++ DynamicProgramming/Medium/TwoKeyboard.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/2-keys-keyboard/
+import java.util.*;
+public class TwoKeyboard{
+        public int minSteps(int n) {
+        int ans=0;
+        for(int i=2;i*i<=n;){
+            if(n%i==0){
+                ans+=i;
+                n/=i;
+            }
+            else i++;
+        }
+        if(n!=1)ans+=n;
+        return ans;
+        
+    }
+}
