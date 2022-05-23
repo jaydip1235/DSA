6b3399d9c4a894d3b04b0d2a1b7bcaa58e4a0a29
jaydip1235 <jaydipdeyalok@gmail.com>
2022-05-24 00:33:26 +0530

    Count of palindromic subsequences

---
+++ DynamicProgramming/Hard/NoOfPalindromicSubsequence.java
@@ -0,0 +1,28 @@
+// https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/dynamic-programming/cps-official/ojquestion
+import jana.util.*;
+import java.io.*;
+public class NoOfPalindromicSubsequence {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int[][] dp = new int[str.length()][str.length()];
+        for (int g = 0; g < dp.length; g++) {
+            for (int i = 0, j = g; j < dp[0].length; i++, j++) {
+                if (g == 0) {
+                    dp[i][j] = 1;
+                } else if (g == 1) {
+                    dp[i][j] = str.charAt(i) == str.charAt(j) ? 3 : 2;
+                } else {
+                    if (str.charAt(i) == str.charAt(j)) {
+                        dp[i][j] = dp[i + 1][j] + dp[i][j - 1] + 1;
+                    } else {
+                        dp[i][j] = dp[i + 1][j] + dp[i][j - 1] - dp[i + 1][j - 1];
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[0][dp[0].length - 1]);
+    }
+}
