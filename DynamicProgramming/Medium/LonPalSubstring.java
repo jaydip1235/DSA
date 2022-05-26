3295d75ac5bc46157b67f5c8078570ec43ff54c7
jaydip1235 <jaydipdeyalok@gmail.com>
2022-05-26 09:37:42 +0530

    longest palindromic substring

---
+++ DynamicProgramming/Medium/LonPalSubstring.java
@@ -0,0 +1,30 @@
+public class LonPalSubstring {
+    public String longestPalindrome(String s) {
+        int len = 0;
+        int si = 0, ei = 0;
+        boolean dp[][] = new boolean[s.length()][s.length()];
+        for (int g = 0; g < s.length(); g++) {
+            for (int i = 0, j = g; j < s.length(); i++, j++) {
+                if (g == 0) {
+                    dp[i][j] = true;
+
+                } else if (g == 1) {
+                    if (s.charAt(j) == s.charAt(i)) {
+                        dp[i][j] = true;
+
+                    }
+                } else {
+                    if (s.charAt(j) == s.charAt(i) && dp[i + 1][j - 1] == true) {
+                        dp[i][j] = true;
+                    }
+                }
+                if (dp[i][j] && g + 1 > len) {
+                    si = i;
+                    ei = j;
+                    len = g + 1;
+                }
+            }
+        }
+        return s.substring(si, ei + 1);
+    }
+}
