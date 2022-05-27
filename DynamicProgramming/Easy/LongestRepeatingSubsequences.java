1b441b5db27f9279afcb728b4f2d72374b884e3e
jaydip1235 <jaydipdeyalok@gmail.com>
2022-05-27 11:20:19 +0530

    longest repeating subsequences

---
+++ DynamicProgramming/Easy/LongestRepeatingSubsequences.java
@@ -0,0 +1,19 @@
+public class LongestRepeatingSubsequences {
+    public int LongestRepeatingSubsequence(String str) {
+        int dp[][] = new int[str.length() + 1][str.length() + 1];
+
+        for (int i = dp.length - 1; i >= 0; i--) {
+            for (int j = dp[0].length - 1; j >= 0; j--) {
+                if (i == dp.length - 1 || j == dp[0].length - 1) {
+                    dp[i][j] = 0;
+                } else if (str.charAt(i) == str.charAt(j) && i!=j) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
