6e2acb9528d4b186b12982de0738a13fe8b2dd02
jaydip1235 <jaydipdeyalok@gmail.com>
2022-06-07 08:30:51 +0530

    Word Break II

---
+++ DynamicProgramming/Hard/WordBreakTwo.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/word-break-ii/
+import jabva.util.*;
+public class WordBreakTwo {
+  HashMap<String,List<String>> map = new HashMap<>();
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        if(map.containsKey(s))return map.get(s);
+        List<String>res = new ArrayList<>();
+        for(String w:wordDict){
+            if(s.length()>=w.length() && s.substring(0,w.length()).equals(w)){
+                if(w.length()==s.length()){
+                    res.add(s);
+                }else{
+                    List<String> temp = wordBreak(s.substring(w.length()),wordDict);
+                    for(String t:temp){
+                        res.add(w+" "+t);
+                    }
+                }
+            }
+        }
+        map.put(s,res);
+        return res;
+    }
+}
