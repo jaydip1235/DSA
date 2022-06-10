35156e23933aa9aeb63ecc9372ba0659d99cd039
jaydip1235 <jaydipdeyalok@gmail.com>
2022-06-10 10:21:06 +0530

    Maximum length of repeated subarray

---
+++ DynamicProgramming/Medium/MaxLenRepeatedSubarray.java
@@ -0,0 +1,18 @@
+
+// https://leetcode.com/problems/maximum-length-of-repeated-subarray/
+public class MaxLenRepeatedSubarray {
+    public int findLength(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        int ans = 0;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+
+        return ans;
+    }
+}
