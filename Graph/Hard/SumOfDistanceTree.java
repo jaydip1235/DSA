c49fdb518036912431ac88be0c76e88af2441142
jaydip1235 <jaydipdeyalok@gmail.com>
2022-08-20 12:22:19 +0530

    sum-of-distances-in-tree

---
+++ Graph/Hard/SumOfDistanceTree.java
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/sum-of-distances-in-tree/
+import java.util.*;
+public class SumOfDistanceTree {
+    private int[] res;
+    private int[] count;
+    private List<HashSet<Integer>> tree;
+
+    public int[] sumOfDistancesInTree(int N, int[][] edges) {
+        tree = new ArrayList<HashSet<Integer>>();
+        res = new int[N];
+        count = new int[N];
+        for (int i = 0; i < N; ++i)
+            tree.add(new HashSet<Integer>());
+        for (int[] e : edges) {
+            tree.get(e[0]).add(e[1]);
+            tree.get(e[1]).add(e[0]);
+        }
+        postOrder(0, -1);
+        preOrder(0, -1);
+        return res;
+    }
+
+    public void postOrder(int root, int pre) {
+        for (int i : tree.get(root)) {
+            if (i == pre)
+                continue;
+            postOrder(i, root);
+            count[root] += count[i];
+            res[root] += res[i] + count[i];
+        }
+        count[root]++;
+    }
+
+    public void preOrder(int root, int pre) {
+        for (int i : tree.get(root)) {
+            if (i == pre)
+                continue;
+            res[i] = res[root] - count[i] + count.length - count[i];
+            preOrder(i, root);
+        }
+    }
+}
