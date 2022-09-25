2c741d07fb8e07de6b6df4fb8802a1b467787b7c
jaydip1235 <jaydipdeyalok@gmail.com>
2022-09-25 23:33:34 +0530

    add

---
+++ Basic&NumberTheory/Easy/Add.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/add-two-integers/
+#include<bits/stdc++.h>
+class Solution
+{
+public
+    int sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
