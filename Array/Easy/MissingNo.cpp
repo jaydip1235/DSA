465495c6681dd81614f42960ddfcb57c6d1deb0a
jaydip1235 <jaydipdeyalok@gmail.com>
2023-01-03 01:46:21 +0530

    missing no

---
+++ Array/Easy/MissingNo.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/missing-number/
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum=((n)*(n+1))/2;
+        for(int i=0;i<nums.size();i++){
+            sum-=nums[i];
+        }
+        return sum;
+    }
+};
