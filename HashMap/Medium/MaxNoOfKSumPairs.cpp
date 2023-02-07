420faf7de425cc059b136e4b3848249d74d8caf2
jaydip1235 <jaydipdeyalok@gmail.com>
2023-02-07 20:59:17 +0530

    max-number-of-k-sum-pairs

---
+++ HashMap/Medium/MaxNoOfKSumPairs.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/max-number-of-k-sum-pairs/
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+      unordered_map<int,int>mp;
+        int count=0;
+    for(auto x:nums)
+    {
+        if(mp[k-x])
+        {
+            mp[(k-x)]--;
+            mp[x]--;
+            count++;
+        }
+        mp[x]++;
+    }
+        return count;
+    }
+};
