ec4536700a7e5c42c2797696ba001ef8b822fb02
jaydip1235 <jaydipdeyalok@gmail.com>
2023-08-24 11:10:38 +0530

    Fizzbuzz

---
+++ Array/Easy/FizzBuzz.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/fizz-buzz/description/
+#include<bits/stdc++.h>
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>ans;
+
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                ans.push_back("FizzBuzz");
+            }else if(i%3==0){
+                  ans.push_back("Fizz");
+            }else if(i%5==0){
+                  ans.push_back("Buzz");
+            }else{
+                ans.push_back(to_string(i));
+            }
+        }
+        return ans;
+    }
+};
