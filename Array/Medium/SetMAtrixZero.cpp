ac956112cec58f9c081ba8393794960303d58462
jaydip1235 <jaydipdeyalok@gmail.com>
2023-08-24 16:55:29 +0530

    set-matrix-zeroes

---
+++ Array/Medium/SetMAtrixZero.cpp
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/set-matrix-zeroes/description/
+#include<bits/stdc++.h>
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+    int col0 = 1;
+    // step 1: Traverse the matrix and
+    // mark 1st row & col accordingly:
+    int n=matrix.size();
+    int m=matrix[0].size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                // mark i-th row:
+                matrix[i][0] = 0;
+
+                // mark j-th column:
+                if (j != 0)
+                    matrix[0][j] = 0;
+                else
+                    col0 = 0;
+            }
+        }
+    }
+
+    // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            if (matrix[i][j] != 0) {
+                // check for col & row:
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    //step 3: Finally mark the 1st col & then 1st row:
+    if (matrix[0][0] == 0) {
+        for (int j = 0; j < m; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+    if (col0 == 0) {
+        for (int i = 0; i < n; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+    }
+};
