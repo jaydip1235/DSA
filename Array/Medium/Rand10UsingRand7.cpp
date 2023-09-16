66d3e3f45fe2a0e46ef8e68a96d205638cc6463d
jaydip1235 <jaydipdeyalok@gmail.com>
2023-09-16 16:21:51 +0530

    implement-rand10-using-rand7

---
+++ Array/Medium/Rand10UsingRand7.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/implement-rand10-using-rand7/
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int rand10() {
+        int temp=(rand7()-1)*7+(rand7());
+        if(temp>=41)return rand10();
+        return temp%10+1;
+    }
+};
