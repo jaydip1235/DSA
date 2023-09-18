d3f125551aa6c5ac7183f4231b07a3b500c71546
jaydip1235 <jaydipdeyalok@gmail.com>
2023-09-18 17:44:20 +0530

    .

--- Array/Medium/Rand10UsingRand7.cpp
+++ Array/Medium/Rand10UsingRand7.cpp
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/implement-rand10-using-rand7/
 #include<bits/stdc++.h>
 class Solution {
-public:
+public: 
     int rand10() {
         int temp=(rand7()-1)*7+(rand7());
         if(temp>=41)return rand10();
