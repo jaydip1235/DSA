d111c607184d358b5751f629554c0dbee17276d1
jaydip1235 <jaydipdeyalok@gmail.com>
2023-09-27 22:14:41 +0530

    decoded-string-at-index

---
+++ Strings/Medium/DecodeStringAtIndex.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/decoded-string-at-index
+#include<bits/stdc++.h>
+class Solution {
+public:
+        string decodeAtIndex(string S, int K) {
+        long N = 0, i;
+        for (i = 0; N < K; ++i)
+            N = isdigit(S[i]) ? N * (S[i] - '0') : N + 1;
+        while (i--)
+            if (isdigit(S[i]))
+                N /= S[i] - '0', K %= N;
+            else if (K % N-- == 0)
+                return string(1, S[i]);
+        return "lee215";
+    }
+};
