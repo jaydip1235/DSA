abb8e10353fdde9493f012060eb03e7ee81698c2
jaydip1235 <jaydipdeyalok@gmail.com>
2023-10-07 23:06:10 +0530

    .

--- Strings/Medium/DecodeStringAtIndex.cpp
+++ Strings/Medium/DecodeStringAtIndex.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
         string decodeAtIndex(string S, int K) {
         long N = 0, i;
-        for (i = 0; N < K; ++i)
+        for (i = 0; N < K; ++i )
             N = isdigit(S[i]) ? N * (S[i] - '0') : N + 1;
         while (i--)
             if (isdigit(S[i]))
