10929f7b418e0e30acced80ad4148ebbb9d26fcc
jaydip1235 <jaydipdeyalok@gmail.com>
2023-10-13 23:20:37 +0530

    min-cost-climbing-stairs

---
+++ DynamicProgramming/Easy/MinCostClimbingStairs.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/min-cost-climbing-stairs/
+#include<bits/stdc++.h>
+class Solution {
+public:
+ int minCostClimbingStairs(vector<int>& cost) {
+        for(int i=2;i<cost.size();i++)
+        {
+            cost[i]+=min(cost[i-1],cost[i-2]);
+        }
+        return min(cost[cost.size()-1],cost[cost.size()-2]);
+    }
+};
